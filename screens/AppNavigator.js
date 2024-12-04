4d96fd588571935a29158a4358dc13614bfe9a29
blackheartHN <nguyentran55it@gmail.com>
2024-12-04 17:29:57 +0700

    Update imports for AppNavigation

--- screens/AppNavigator.js
+++ screens/AppNavigator.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { CartProvider } from '../screens/context/CartContext';  
 import HomePage from '../screens/HomePage';
 import CartScreen from '../screens/CartScreen';
 import PaymentScreen from '../screens/PaymentScreen';
@@ -14,43 +15,45 @@ const Stack = createStackNavigator();
 export default function AppNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="HomePage">
-        <Stack.Screen
-          name="HomePage"
-          component={HomePage}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="CartScreen"
-          component={CartScreen}
-          options={{ title: 'Cart' }}
-        />
-        <Stack.Screen
-          name="PaymentScreen"
-          component={PaymentScreen}
-          options={{ title: 'Checkout' }}
-        />
-        <Stack.Screen
-          name="ReviewScreen"
-          component={ReviewScreen}
-          options={{ title: 'Reviews' }}
-        />
-        <Stack.Screen
-          name="WriteReviewScreen"
-          component={WriteReviewScreen}
-          options={{ title: 'Write Review' }}
-        />
-        <Stack.Screen
-          name="CategoryScreen"
-          component={CategoryScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="ProductScreen"
-          component={ProductScreen}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
+      <CartProvider>
+        <Stack.Navigator initialRouteName="HomePage">
+          <Stack.Screen
+            name="HomePage"
+            component={HomePage}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="CartScreen"
+            component={CartScreen}
+            options={{ title: 'Cart' }}
+          />
+          <Stack.Screen
+            name="PaymentScreen"
+            component={PaymentScreen}
+            options={{ title: 'Checkout' }}
+          />
+          <Stack.Screen
+            name="ReviewScreen"
+            component={ReviewScreen}
+            options={{ title: 'Reviews' }}
+          />
+          <Stack.Screen
+            name="WriteReviewScreen"
+            component={WriteReviewScreen}
+            options={{ title: 'Write Review' }}
+          />
+          <Stack.Screen
+            name="CategoryScreen"
+            component={CategoryScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="ProductScreen"
+            component={ProductScreen}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </CartProvider>
     </NavigationContainer>
   );
 }
