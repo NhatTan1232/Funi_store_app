5e7bf0fe9fb6609dfc4d05a052c61e3ae356a312
blackheartHN <nguyentran55it@gmail.com>
2024-12-04 17:29:26 +0700

    Added CartContext

---
+++ screens/context/CartContext.js
@@ -0,0 +1,33 @@
+import React, { createContext, useState } from 'react';
+
+export const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart((prevCart) => {
+      const existingProductIndex = prevCart.findIndex(
+        (item) => item.id === product.id && item.selectedColor.color_name === product.selectedColor.color_name
+      );
+
+      if (existingProductIndex !== -1) {
+        const updatedCart = [...prevCart];
+        updatedCart[existingProductIndex].quantity += 1;
+        return updatedCart;
+      } else {
+        return [...prevCart, { ...product, quantity: 1 }];
+      }
+    });
+  };
+
+  const removeFromCart = (productId) => {
+    setCart((prevCart) => prevCart.filter((item) => item.id !== productId));
+  };
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart, setCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
